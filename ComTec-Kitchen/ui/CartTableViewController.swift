17ca874cbdcb2fb7f2ba62537d4d9f34f09e75c7
Adrian Kunz <clashsoft@hotmail.com>
2019-02-24 00:59:26 +0100

    Implement Clear and Submit Cart

--- ComTec-Kitchen/ui/CartTableViewController.swift
+++ ComTec-Kitchen/ui/CartTableViewController.swift
@@ -18,6 +18,49 @@ class CartTableViewController: DictTableViewController<Purchase> {
 		return ["": Cart.shared.purchases]
 	}
 
+	@IBAction func submitClicked(_ sender: Any) {
+		let totalAmount = Cart.shared.getTotalAmount()
+		if totalAmount == 0 {
+			return
+		}
+
+		let total = Cart.shared.getTotal()
+		let title = "Buy"
+		let message = "Are you sure you want to buy \(totalAmount) item\(totalAmount == 1 ? "" : "s") for \(total.€) ?"
+
+		let dialog = UIAlertController(title: title, message: message, preferredStyle: .alert)
+
+		dialog.addAction(UIAlertAction(title: "Yes", style: .default) { (_) in
+			Cart.shared.submit()
+			self.refresh()
+		})
+
+		dialog.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+
+		self.present(dialog, animated: true, completion: nil)
+	}
+
+	@IBAction func clearClicked(_ sender: Any) {
+		let totalAmount = Cart.shared.getTotalAmount()
+		if totalAmount == 0 {
+			return
+		}
+
+		let title = "Clear Cart"
+		let message = "Are you sure you want to remove \(totalAmount) item\(totalAmount == 1 ? "" : "s") from the cart?"
+
+		let dialog = UIAlertController(title: title, message: message, preferredStyle: .alert)
+
+		dialog.addAction(UIAlertAction(title: "Yes", style: .destructive) { (_) in
+			Cart.shared.clear()
+			self.refresh()
+		})
+
+		dialog.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+
+		self.present(dialog, animated: true, completion: nil)
+	}
+
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: "cartCell", for: indexPath)
 		let purchase = getItem(at: indexPath)
