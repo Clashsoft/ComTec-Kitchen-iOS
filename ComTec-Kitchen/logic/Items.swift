f55e94cf5dd33b16b7f695833602ef7061483b92
Adrian Kunz <clashsoft@hotmail.com>
2019-02-23 21:33:45 +0100

    Add Items and Users

---
+++ ComTec-Kitchen/logic/Items.swift
@@ -0,0 +1,84 @@
+//
+// Created by Adrian Kunz on 2019-02-23.
+// Copyright (c) 2019 Clashsoft. All rights reserved.
+//
+
+import Foundation
+
+class Items {
+	// =============== Static Fields ===============
+
+	static let shared = Items()
+
+	// =============== Fields ===============
+
+	private let api = KitchenAPI.shared
+
+	private var items: [String:Item] = [:]
+
+	// --------------- Access ---------------
+
+	func get(id: String) -> Item? {
+		return items[id]
+	}
+
+	func exists(id: String) -> Bool {
+		return items.keys.contains(id)
+	}
+
+	func getAll() -> [Item] {
+		return Array(items.values)
+	}
+
+	func getGrouped() -> [String:[Item]] {
+		return Dictionary(grouping: items.values) { $0.kind }
+	}
+
+	// --------------- Modification ---------------
+
+	func updateLocal(item: Item) {
+		items[item._id] = item
+	}
+
+	func deleteLocal(item: Item) {
+		items.removeValue(forKey: item._id)
+	}
+
+	// --------------- Communication ---------------
+
+	func refreshAll() {
+		items.removeAll()
+		api.getAllItems { (data, error) in
+			if let json = data, let items = JSONTranslator.json2items(json: json) {
+				items.forEach(self.updateLocal)
+			}
+		}
+	}
+
+	func create(item: Item) {
+		guard let itemJson = JSONTranslator.item2json(item: item) else {
+			return
+		}
+		api.createItem(json: itemJson) { (data, error) in
+			if let json = data, let item = JSONTranslator.json2item(json: json) {
+				self.updateLocal(item: item)
+			}
+		}
+	}
+
+	func update(item: Item) {
+		guard let json = JSONTranslator.item2json(item: item) else {
+			return
+		}
+		api.updateItem(id: item._id, json: json) { (data, error) in
+			if let json = data, let item = JSONTranslator.json2item(json: json) {
+				self.updateLocal(item: item)
+			}
+		}
+	}
+
+	func delete(item: Item) {
+		api.deleteItem(id: item._id, completion: ignoreResult)
+		deleteLocal(item: item)
+	}
+}
---
+++ ComTec-Kitchen/logic/Users.swift
@@ -0,0 +1,77 @@
+//
+// Created by Adrian Kunz on 2019-02-23.
+// Copyright (c) 2019 Clashsoft. All rights reserved.
+//
+
+import Foundation
+
+class Users {
+	// =============== Static Fields ===============
+
+	static let shared = Users()
+
+	// =============== Fields ===============
+
+	private let api = KitchenAPI.shared
+
+	private var users: [String:User] = [:]
+
+	// =============== Methods ===============
+
+	// --------------- Access ---------------
+
+	func get(id: String) -> User? {
+		return users[id]
+	}
+
+	func getAll() -> [User] {
+		return Array(users.values)
+	}
+
+	func getGrouped() -> [String:[User]] {
+		return Dictionary(grouping: users.values) { "\($0.name.first ?? " ")" }
+	}
+
+	// --------------- Modification ---------------
+
+	func updateLocal(user: User) {
+		users[user._id] = user
+	}
+
+	func deleteLocal(user: User) {
+		users.removeValue(forKey: user._id)
+	}
+
+	// --------------- Communication ---------------
+
+	func refreshAll() {
+		users.removeAll()
+		api.getAllUsers { (data, error) in
+			if let json = data, let users = JSONTranslator.json2users(json: json) {
+				users.forEach(self.updateLocal)
+			}
+		}
+	}
+
+	func refresh(user: User) {
+		api.getUser(id: user._id) { (data, error) in
+			if let json = data, let user = JSONTranslator.json2user(json: json) {
+				self.updateLocal(user: user)
+			}
+		}
+	}
+
+	func update(user: User) {
+		guard let json = JSONTranslator.user2json(user: user) else {
+			return
+		}
+
+		api.updateUser(id: user._id, json: json, completion: ignoreResult)
+		updateLocal(user: user)
+	}
+
+	func delete(user: User) {
+		api.deleteUser(id: user._id, completion: ignoreResult)
+		deleteLocal(user: user)
+	}
+}
