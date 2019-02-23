a28735517021863ddb873d64bfa637f60973f72d
Adrian Kunz <clashsoft@hotmail.com>
2019-02-23 17:12:49 +0100

    Initial Commit

---
+++ ComTec-Kitchen/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ComTec-Kitchen
+//
+//  Created by Adrian Kunz on 2019-02-23.
+//  Copyright © 2019 Clashsoft. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
---
+++ ComTec-Kitchen/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ComTec-Kitchen
+//
+//  Created by Adrian Kunz on 2019-02-23.
+//  Copyright © 2019 Clashsoft. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
