2e2838cfc192b73a6f75bba88d15b580f47b07f7
Adrian Kunz <clashsoft@hotmail.com>
2019-02-23 23:34:06 +0100

    Add Purchases

---
+++ ComTec-Kitchen/logic/Purchases.swift
@@ -0,0 +1,69 @@
+//
+// Created by Adrian Kunz on 2019-02-23.
+// Copyright (c) 2019 Clashsoft. All rights reserved.
+//
+
+import Foundation
+
+class Purchases {
+	// =============== Static Fields ===============
+
+	static let shared = Purchases()
+
+	// =============== Fields ===============
+
+	let api = KitchenAPI.shared
+
+	var purchases: [String:Purchase] = [:]
+
+	// =============== Methods ===============
+
+	// --------------- Access ---------------
+
+	func getAll() -> [Purchase] {
+		return Array(purchases.values)
+	}
+
+	func getMine() -> [Purchase] {
+		guard let user = Session.shared.getLoggedInUser() else {
+			return []
+		}
+		let userId = user._id
+		return Array(purchases.values.filter { $0.user_id == userId })
+	}
+
+	func getMineGrouped() -> [String:[Purchase]] {
+		return Dictionary(grouping: getMine()) { (purchase: Purchase) in
+			guard let created = purchase.created else {
+				return ""
+			}
+			return String(created[..<created.index(created.startIndex, offsetBy: 10)])
+		}
+	}
+
+	// --------------- Modification ---------------
+
+	func updateLocal(purchase: Purchase) {
+		purchases[purchase._id] = purchase
+	}
+
+	// --------------- Communication ---------------
+
+	func refreshAll(completion: (() -> Void)? = nil) {
+		api.getAllPurchases { (data, error) in
+			if let json = data, let purchases = JSONTranslator.json2purchases(json: json) {
+				purchases.forEach(self.updateLocal)
+				completion?()
+			}
+		}
+	}
+
+	func refreshMine(completion: (() -> Void)? = nil) {
+		api.getMyPurchases { (data, error) in
+			if let json = data, let purchases = JSONTranslator.json2purchases(json: json) {
+				purchases.forEach(self.updateLocal)
+				completion?()
+			}
+		}
+	}
+}
