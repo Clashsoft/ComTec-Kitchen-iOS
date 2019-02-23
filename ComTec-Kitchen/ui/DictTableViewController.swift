6f66b829eb57206f615401b4cf771e807119cea4
Adrian Kunz <clashsoft@hotmail.com>
2019-02-24 00:16:15 +0100

    Implement Purchase History

---
+++ ComTec-Kitchen/ui/DictTableViewController.swift
@@ -0,0 +1,72 @@
+//
+// Created by Adrian Kunz on 2019-02-24.
+// Copyright (c) 2019 Clashsoft. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DictTableViewController<T> : UITableViewController {
+	var dict: [String:[T]] = [:]
+
+	private func dictSection(at section: Int) -> (header: String, items: [T]) {
+		let (key, value) = dict[dict.index(dict.startIndex, offsetBy: section)]
+		return (header: key, items: value)
+	}
+
+	func getItem(at indexPath: IndexPath) -> T {
+		return dictSection(at: indexPath.section).items[indexPath.row]
+	}
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Uncomment the following line to preserve selection between presentations
+		// self.clearsSelectionOnViewWillAppear = false
+
+		// Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+		// self.navigationItem.rightBarButtonItem = self.editButtonItem
+
+		self.refreshControl = UIRefreshControl()
+		self.refreshControl?.addTarget(self, action: #selector(handleRefresh(_:)), for: .primaryActionTriggered)
+
+		self.refresh()
+	}
+
+	@objc func handleRefresh(_ refreshControl: UIRefreshControl) {
+		refresh()
+	}
+
+	func refresh() {
+		UIApplication.shared.isNetworkActivityIndicatorVisible = true
+		refresh {
+			DispatchQueue.main.async {
+				self.dict = self.getDict()
+				self.tableView.reloadData()
+				self.tableView.refreshControl?.endRefreshing()
+				UIApplication.shared.isNetworkActivityIndicatorVisible = false
+			}
+		}
+	}
+
+	func getDict() -> [String:[T]] {
+		fatalError("not implemented")
+	}
+
+	func refresh(completion: @escaping () -> Void) {
+		fatalError("not implemented")
+	}
+
+	override func numberOfSections(in tableView: UITableView) -> Int {
+		return dict.count
+	}
+
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return dictSection(at: section).items.count
+	}
+
+	override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+		return dictSection(at: section).header
+	}
+
+}
---
+++ ComTec-Kitchen/ui/PurchasesTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PurchasesTableViewController.swift
+//  ComTec-Kitchen
+//
+//  Created by Adrian Kunz on 2019-02-23.
+//  Copyright © 2019 Clashsoft. All rights reserved.
+//
+
+import UIKit
+
+class PurchasesTableViewController: DictTableViewController<Purchase> {
+	override func refresh(completion: @escaping () -> Void) {
+		Purchases.shared.refreshMine(completion: completion)
+	}
+
+	override func getDict() -> [String: [Purchase]] {
+		return Purchases.shared.getMineGrouped()
+	}
+
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "purchaseCell", for: indexPath)
+		let purchase = getItem(at: indexPath)
+		let itemName = Items.shared.get(id: purchase.item_id)?.name ?? purchase.item_id
+
+		cell.textLabel?.text = "\(itemName)"
+		cell.detailTextLabel?.text = "\(purchase.created ?? "") | \(purchase.amount) * \(purchase.itemPrice) = \(purchase.total)"
+
+		return cell
+	}
+}
